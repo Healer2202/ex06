19bd9197998113c0de534406e26e2546c53207fd
nguyenthihongngoc <onepice02062000@gmail.com>
2019-10-15 15:26:22 +0700

    ex06

---
+++ ex06/Problem1/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    public class Account
+    {
+        public Account(int amount)
+        {
+
+        }
+        public void Withdraw(int amount)
+        {
+
+        }
+        public void Deposit(int amount)
+        {
+
+        }
+        public int Balance()
+        {
+            return 200000;
+        }
+    }
+}
