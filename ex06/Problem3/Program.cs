5816a2060b255a674b2e3ad3d8fbebe56a8ec176
Healer2202 <0962392354Hl>
2019-10-29 11:59:28 +0700

    ex06

--- ex06/Problem3/Program.cs
+++ ex06/Problem3/Program.cs
@@ -10,6 +10,36 @@ namespace Problem3
     {
         static void Main(string[] args)
         {
+            queue user = new queue(10);
+            user.Enqueue(1);
+            user.Enqueue(2);
+            user.Enqueue(3);
+            user.Enqueue(4);
+            user.Enqueue(5);
+            Console.WriteLine(@"----The count at begin when user push ell");
+            user.checkCount();
+            //  user Dequeue 1 element 
+            Console.WriteLine(@"----Dequeue-----");
+            user.Dequeue();
+            Console.WriteLine(@"----Check count after Dequeue----");
+            /*  check count after dequeue ( the count will -1 element after Dequeue )
+             *  when user Dequeue ele that ele will go out the Queue 
+             */
+            user.checkCount();
+            //  user wanna peek in queue
+            Console.WriteLine(@"----Peek----");
+            user.peek();
+            //  peek different than Dequeue because : when user peek the count not change;
+            Console.WriteLine(@"-----Check count afete Peek-----");
+            user.checkCount();
+            // push 1 ele 
+            user.Enqueue(6);
+            user.Enqueue(7);
+            user.Enqueue(8);
+            user.Enqueue(9);
+            Console.WriteLine(@"-----The count after Enqueue-----");
+            // then check count ? is it changing ?
+            user.checkCount();
         }
     }
 }
