f329c7b2da3ea32040d27711a9f2be8fb8bcfc9d
NguyenHoangLong <55733418+Healer2202@users.noreply.github.com>
2019-10-29 12:04:44 +0700

    Add files via upload

---
+++ ex06/Problem3/MyQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    public class MyQueue
+    {
+        private int[] space;
+        private int count = 0;
+        private int ele;
+        private int statue = 0;
+        static void Main(string[] args)
+        {
+        }
+        public MyQueue(int number)
+        {
+            space = new int[number];
+        }
+        public void Enqueue(int number)
+        {
+            ele = number;
+            space[count] = ele;
+            count++;
+            statue++;
+        }
+        public int Dequeue()
+        {
+            int temp = count;
+            count--;
+            Console.WriteLine(space[statue - temp]);
+            return space[statue - temp];
+
+        }
+        public int peek()
+        {
+            Console.WriteLine(space[statue - count]);
+            return space[statue - count];
+        }
+        public int checkCount()
+        {
+            Console.WriteLine(count);
+            return count;
+        }
+        public void print()
+        {
+            for (int i = 0; i < space.Length; i++)
+            {
+                Console.Write(space[i]);
+            }
+        }
+    }
+}
